cfbe5f4caf798d480ce85ec7be10a78cd4fcbc2a
Andrew Harmel-Law <andrew.harmel-law@capgemini.com>
2016-03-09 08:26:46 +0000

    Half-way to getting Metrics Servlet Config to work

--- src/main/java/com/capgemini/brahma/Application.java
+++ src/main/java/com/capgemini/brahma/Application.java
@@ -4,9 +4,6 @@ import org.apache.camel.spring.boot.FatJarRouter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-
-import com.capgemini.camel.metrics.publisher.configuration.MetricsServletConfiguration;
 
 //@Import({MetricsServletConfiguration.class})
 @SpringBootApplication
