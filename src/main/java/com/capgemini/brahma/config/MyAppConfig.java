5383792495c79f1fa95afc879419e24fdc08674e
Andrew Harmel-Law <andrew.harmel-law@capgemini.com>
2016-03-10 09:17:34 +0000

    Put archaius-spring-adapter in, and configured it using Java config (not XML - therefore could remove META-INF file)

--- src/main/java/com/capgemini/brahma/config/MyAppConfig.java
+++ src/main/java/com/capgemini/brahma/config/MyAppConfig.java
@@ -5,10 +5,13 @@ import org.apache.camel.spring.boot.CamelContextConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+import com.capgemini.archaius.spring.ArchaiusBridgePropertyPlaceholderConfigurer;
 
 @Configuration
-//@ImportResource("classpath:META-INF/spring/properties-config.xml")
+
 public class MyAppConfig {
 
     @Autowired
@@ -22,5 +25,25 @@ public class MyAppConfig {
                 System.out.println("your custom configuration goes here.");
             }
         };
+    }
+
+    @Bean
+    public ArchaiusBridgePropertyPlaceholderConfigurer bridgePropertyPlaceholder() {
+
+        ArchaiusBridgePropertyPlaceholderConfigurer configurer = new ArchaiusBridgePropertyPlaceholderConfigurer();
+
+        configurer.setIgnoreResourceNotFound(true);
+        configurer.setInitialDelayMillis(5000);
+        configurer.setDelayMillis(5000);
+        configurer.setIgnoreDeletesFromSource(true);
+        Resource[] resources = new Resource[5];
+        resources[0] = new FileSystemResource("config/camel.properties");
+        resources[1] = new FileSystemResource("config/env.properties");
+        resources[2] = new FileSystemResource("config/hystrix.properties");
+        resources[3] = new FileSystemResource("config/metrics.properties");
+        resources[4] = new FileSystemResource("config/application.properties");
+        configurer.setLocations(resources);
+
+        return configurer;
     }
 }
