db622be3365a37332eeb93596f1f1eb87497254f
Nikhil Vibhav <nikhil.vibhav@capgemini.com>
2017-01-13 12:50:28 +0530

    Update Spring Boot to 1.4.3.RELEASE and Camel to 2.18.1

--- src/main/java/com/capgemini/brahma/config/CamelHttpServletConfig.java
+++ src/main/java/com/capgemini/brahma/config/CamelHttpServletConfig.java
@@ -4,7 +4,7 @@ import org.apache.camel.component.servlet.CamelHttpTransportServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
